e7ba7ebdcbb8f738bff020975a94328543ab596d
Liquidzk <492928615@qq.com>
2023-12-06 19:53:30 +0800

    codes can be loaded by android

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,11 @@ export default defineConfig({
       },
     },
   },
+  build:{
+    rollupOptions:{
+      external:['vuetify/labs/VDatePicker'],
+    },
+  },
   css: {
     loaderOptions: {
       sass: {
